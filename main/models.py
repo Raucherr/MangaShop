3e822e60a0ea4837a1aef1840f1ba4209fc27c4f
Raucherr <shnicelv@gmail.com>
2022-03-29 16:29:19 +0600

    commit

--- main/models.py
+++ main/models.py
@@ -1,12 +1,12 @@
-# from django.db import models
-#
-#
-# class Genre(models.Model):
-#     slug = models.SlugField(primary_key=True)
-#     name = models.CharField(max_length=55, unique=True)
-#
-#     def __str__(self):
-#         return self.name
+from django.db import models
+
+
+class Genre(models.Model):
+    slug = models.SlugField(primary_key=True)
+    name = models.CharField(max_length=55, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Manga(models.Model):
